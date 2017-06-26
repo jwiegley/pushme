3905c854d0dbf3af595c097a034e7a49136a551b
John Wiegley <johnw@newartisans.com>
2017-06-26 11:32:47 -0700

    Use hpack

--- default.nix
+++ default.nix
@@ -1,24 +1,24 @@
-{ mkDerivation, aeson, base, bytestring, pipes, pipes-group, pipes-safe
-, pipes-text, containers, io-storage, lens, logging, monad-logger
-, old-locale, optparse-applicative, parallel-io, regex-posix, safe
-, shelly, stdenv, system-fileio, system-filepath, temporary, text
-, text-format, time, transformers, unix, unordered-containers, yaml
-, foldl
+{ mkDerivation, aeson, base, bytestring, containers, foldl
+, io-storage, lens, logging, monad-logger, old-locale
+, optparse-applicative, parallel-io, pipes, pipes-group, pipes-safe
+, pipes-text, regex-posix, safe, shelly, stdenv, system-fileio
+, system-filepath, temporary, text, text-format, time, transformers
+, unix, unordered-containers, yaml
 }:
 mkDerivation {
   pname = "pushme";
-  version = "2.0.1.0";
+  version = "2.1.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  buildDepends = [
-    aeson base bytestring pipes pipes-group pipes-safe pipes-text containers
-    io-storage lens logging monad-logger old-locale optparse-applicative
-    parallel-io regex-posix safe shelly system-fileio system-filepath
-    temporary text text-format time transformers unix unordered-containers
-    yaml foldl
+  executableHaskellDepends = [
+    aeson base bytestring containers foldl io-storage lens logging
+    monad-logger old-locale optparse-applicative parallel-io pipes
+    pipes-group pipes-safe pipes-text regex-posix safe shelly
+    system-fileio system-filepath temporary text text-format time
+    transformers unix unordered-containers yaml
   ];
-  homepage = "https://github.com/jwiegley/pushme";
+  homepage = "https://github.com/jwiegley/pushme#readme";
   description = "Tool to synchronize directories with rsync, zfs or git-annex";
   license = stdenv.lib.licenses.bsd3;
 }
