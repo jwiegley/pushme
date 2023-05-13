57ad0c149b2ea06292d3a08e9008087ce4d624f2
John Wiegley <johnw@newartisans.com>
2023-05-13 16:36:19 -0700

    Add flake.nix file

--- default.nix
+++ default.nix
@@ -1,43 +1,7 @@
-{ compiler    ? "ghc8107"
-, doBenchmark ? false
-, doTracing   ? false
-, doStrict    ? false
-, rev         ? "faad370edcb37162401be50d45526f52bb16a713"
-, sha256      ? "1d82d4vh0layf6n925j0h2nym16jbvcvps3l5m8ln9hxn0m6gadn"
-, pkgs        ? import (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-    inherit sha256; }) {
-    config.allowUnfree = true;
-    config.allowBroken = true;
-  }
-, returnShellEnv ? pkgs.lib.inNixShell
-, mkDerivation ? null
-}:
-
-let haskellPackages = pkgs.haskell.packages.${compiler};
-
-in haskellPackages.developPackage {
-  root = ./.;
-
-  overrides = self: super: with pkgs.haskell.lib; {
-    pipes-text      = markUnbroken (doJailbreak super.pipes-text);
-    time-compat     = doJailbreak super.time-compat;
-    time-recurrence = unmarkBroken (doJailbreak super.time-recurrence);
-  };
-
-  source-overrides = {
-  };
-
-  modifier = drv: pkgs.haskell.lib.overrideCabal
-   (pkgs.haskell.lib.justStaticExecutables drv) (attrs: {
-    inherit doBenchmark;
-    buildTools = (attrs.buildTools or []) ++ [
-      haskellPackages.cabal-install
-      haskellPackages.hpack
-      haskellPackages.hoogle
-      haskellPackages.hasktags
-    ];
-  });
-
-  inherit returnShellEnv;
-}
+(import (
+  fetchTarball {
+    url = "https://github.com/edolstra/flake-compat/archive/35bb57c0c8d8b62bbfd284272c928ceb64ddbde9.tar.gz";
+    sha256 = "1prd9b1xx8c0sfwnyzkspplh30m613j42l1k789s521f4kv4c2z2"; }
+) {
+  src =  ./.;
+}).defaultNix
