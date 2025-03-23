1b434d14e3124d8639f8c3e470f7284a5efc30f4
John Wiegley <johnw@newartisans.com>
2025-03-23 15:54:00 -0700

    Reformat Options.hs

--- Pushme/Options.hs
+++ Pushme/Options.hs
@@ -15,93 +15,112 @@ copyright = "2013-4"
 
 pushmeSummary :: String
 pushmeSummary =
-    "pushme " ++ version ++ ", (C) " ++ copyright ++ " John Wiegley"
+  "pushme " ++ version ++ ", (C) " ++ copyright ++ " John Wiegley"
 
 data Options = Options
-    { jobs      :: Int
-    , dryRun    :: Bool
-    , noSync    :: Bool
-    , copyAll   :: Bool
-    , dump      :: Bool
-    , ssh       :: String
-    , rsyncOpt  :: String
-    , checksum  :: Bool
-    , fromName  :: String
-    , configDir :: String
-    , filesets  :: String
-    , classes   :: String
-    , siUnits   :: Bool
-    , verbose   :: Bool
-    , quiet     :: Bool
-    , cliArgs   :: [String]
-    }
-    deriving (Data, Typeable, Show, Eq)
+  { jobs :: Int,
+    dryRun :: Bool,
+    noSync :: Bool,
+    copyAll :: Bool,
+    dump :: Bool,
+    ssh :: String,
+    rsyncOpt :: String,
+    checksum :: Bool,
+    fromName :: String,
+    configDir :: String,
+    filesets :: String,
+    classes :: String,
+    siUnits :: Bool,
+    verbose :: Bool,
+    quiet :: Bool,
+    cliArgs :: [String]
+  }
+  deriving (Data, Typeable, Show, Eq)
 
 pushmeOpts :: Parser Options
-pushmeOpts = Options
-    <$> option auto
-        (   short 'j'
-         <> long "jobs"
-         <> value 1
-         <> help "Run INT concurrent finds at once (default: 1)")
+pushmeOpts =
+  Options
+    <$> option
+      auto
+      ( short 'j'
+          <> long "jobs"
+          <> value 1
+          <> help "Run INT concurrent finds at once (default: 1)"
+      )
     <*> switch
-        (   short 'n'
-         <> long "dry-run"
-         <> help "Don't take any actions")
+      ( short 'n'
+          <> long "dry-run"
+          <> help "Don't take any actions"
+      )
     <*> switch
-        (   short 'N'
-         <> long "no-sync"
-         <> help "Don't even attempt a dry-run sync")
+      ( short 'N'
+          <> long "no-sync"
+          <> help "Don't even attempt a dry-run sync"
+      )
     <*> switch
-        (   long "copy-all"
-         <> help "For git-annex directories, copy all files")
+      ( long "copy-all"
+          <> help "For git-annex directories, copy all files"
+      )
     <*> switch
-        (   long "dump"
-         <> help "Show all the stores that would be synced")
+      ( long "dump"
+          <> help "Show all the stores that would be synced"
+      )
     <*> strOption
-        (   long "ssh"
-         <> value ""
-         <> help "Use a specific ssh command")
+      ( long "ssh"
+          <> value ""
+          <> help "Use a specific ssh command"
+      )
     <*> strOption
-        (   long "rsync"
-         <> value ""
-         <> help "Use a specific rsync command")
+      ( long "rsync"
+          <> value ""
+          <> help "Use a specific rsync command"
+      )
     <*> switch
-        (   long "checksum"
-         <> help "Pass --checksum flag to rsync")
+      ( long "checksum"
+          <> help "Pass --checksum flag to rsync"
+      )
     <*> strOption
-        (   long "from"
-         <> value ""
-         <> help "Provide the name of the current host")
+      ( long "from"
+          <> value ""
+          <> help "Provide the name of the current host"
+      )
     <*> strOption
-        (   long "config"
-         <> value "~/.pushme"
-         <> help "Directory containing configuration files (def: ~/.pushme)")
+      ( long "config"
+          <> value "~/.pushme"
+          <> help "Directory containing configuration files (def: ~/.pushme)"
+      )
     <*> strOption
-        (   short 'f'
-         <> long "filesets"
-         <> value ""
-         <> help "Synchronize the given fileset(s) (comma-sep)")
+      ( short 'f'
+          <> long "filesets"
+          <> value ""
+          <> help "Synchronize the given fileset(s) (comma-sep)"
+      )
     <*> strOption
-        (   short 'c'
-         <> long "classes"
-         <> value ""
-         <> help "Filesets classes to synchronize (comma-sep)")
+      ( short 'c'
+          <> long "classes"
+          <> value ""
+          <> help "Filesets classes to synchronize (comma-sep)"
+      )
     <*> switch
-        (   long "si"
-         <> help "Use 1000 instead of 1024 to divide")
+      ( long "si"
+          <> help "Use 1000 instead of 1024 to divide"
+      )
     <*> switch
-        (   short 'v'
-         <> long "verbose"
-         <> help "Report progress verbosely")
+      ( short 'v'
+          <> long "verbose"
+          <> help "Report progress verbosely"
+      )
     <*> switch
-        (   short 'q'
-         <> long "quiet"
-         <> help "Be a little quieter")
-   <*> many (argument (eitherReader Right) (metavar "ARGS"))
-    -- <*> many (argument Just (metavar "ARGS"))
+      ( short 'q'
+          <> long "quiet"
+          <> help "Be a little quieter"
+      )
+    <*> many (argument (eitherReader Right) (metavar "ARGS"))
 
-optionsDefinition = info
+-- <*> many (argument Just (metavar "ARGS"))
+
+optionsDefinition =
+  info
     (helper <*> pushmeOpts)
     (fullDesc <> progDesc "" <> header pushmeSummary)
 
